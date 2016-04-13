96d1d939d6b886ac6d35250a5f52ad6be00e0292
Victor Rodriguez <vix206@gmail.com>
2016-04-13 10:49:17 -0500

    Update scriptGetSpanAccessCount.sh

--- Issue-05-Scripts/scriptGetSpanAccessCount.sh
+++ Issue-05-Scripts/scriptGetSpanAccessCount.sh
@@ -2,14 +2,14 @@
 
 filename="$1"
 
-cut -d \  -f 1 "$filename" > $filename-tmp2
+cut -d \  -f 1 "$filename-dataset" > $filename-tmp2
 tail -n +2 $filename-tmp2 > $filename-tmp
 rm -f $filename-tmp2
 
 echo "id span accessCount"
 
 while read id; do
-	filename_id="Animation-traces/it-$id.txt"
+	filename_id="$filename-interarrivals/it-$id.txt"
 	interarrivals=$(wc -l $filename_id | cut -d \  -f 1)
 	accessCount=$((interarrivals + 1))
 	span=$(awk '{ sum += $1 } END { printf("%.10f\n",sum) }' $filename_id)
