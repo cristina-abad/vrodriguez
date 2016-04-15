a23674d36cf7e56e216be1e8467a4b9fca31dc73
Victor Rodriguez <vix206@gmail.com>
2016-04-15 12:39:30 -0500

    VR - Updated and Optimized
    Use the data.table library that improve time the read file and optimization of the structure code

--- Issue-05-Scripts/R/scriptGetInterarrivalsSummaryStatistics.R
+++ Issue-05-Scripts/R/scriptGetInterarrivalsSummaryStatistics.R
@@ -1,8 +1,8 @@
 #### Add libraries neccesaries
-for (package in c('getopt', 'hash', 'moments')) {
-  if (!require(package, character.only=T, quietly=T)) {
+for (package in c('data.table', 'getopt', 'hash', 'moments')) {
+  if (!require(package, character.only=T, quietly=T, warn.conflicts = F)) {
     install.packages(package)
-    library(package, character.only=T)
+    library(package, character.only=T, quietly = T, warn.conflicts = F)
   }
 }
 
@@ -21,45 +21,51 @@ spec <- t(matrix(c('help', 'h', 0, "logical",
           'file', 'f', 1, "character",
           'tracefile', 't', 2, "character"),nrow=4, ncol=3))
 argv <- commandArgs(trailingOnly = TRUE)
-#argv <- c("-f", "Animation-lookup-100k.txt")
-opts = getopt(spec, argv)
+#args <- c("-f","./test/Animation-lookup-50k.txt")
+opts = getopt(spec, args)
 if(!is.null(opts$help)){
-  print('scriptGetInterarrivalsSummaryStatistics.py -f <tracefile>')
+  print('scriptGetInterarrivalsSummaryStatistics.R -f <tracefile>')
   quit(save = "no", status = 2)
 }else if(!is.null(opts$file) || !is.null(opts$tracefile)){
     traceFilename <- opts$file
 }else{
-  print('scriptGetInterarrivalsSummaryStatistics.py -f <tracefile>')
+  print('scriptGetInterarrivalsSummaryStatistics.R -f <tracefile>')
   quit(save = "no", status = 2)
 }
 if(length(traceFilename) == 0){
   print ('ERROR: Must specify a trace filename.')
-  print ('scriptGetInterarrivalsSummaryStatistics.py -f <tracefile>')
+  print ('scriptGetInterarrivalsSummaryStatistics.R -f <tracefile>')
   quit(save = "no", status = 1)
 }
 
 ### Create the interarrival dictionary
 
-interarrival <- hash() # Create a hash empty
+interarrival <- hash::hash() # Create a hash empty
 
 ### Open the file in read mode, and parse each line to obtain the timestamp,
 ### the command, and the id.
 
-reader <- read.csv(file = traceFilename, sep = " ", header = FALSE)
-for(line in 1:nrow(reader)){
-  write(reader[line,1], file = paste(traceFilename,"-IndiceLeido", sep = ""), append = TRUE) # Add file to log
-  if(!is.null(interarrival[[as.character(reader[line,3])]])){
-    dir.create(paste(traceFilename, "-interarrivals", sep = ""))
-    name <- paste(traceFilename, "-interarrivals/it-", reader[line,3], ".txt", sep = "")
-    cont <- (reader[line,1] * 1000) - interarrival[[as.character(reader[line,3])]]
+# reader <- read.csv(file = traceFilename, sep = " ", header = FALSE)
+reader <- fread(input = traceFilename,  sep = " ", header = FALSE)
+
+mapply(function(id, n){
+  write(id, 
+        file = paste(traceFilename,"-IndiceLeido", sep = ""), 
+        append = TRUE) # Add file to log
+  if(!is.null(interarrival[[ n ]])){
+    #dir.create(paste(traceFilename, "-interarrivals", sep = ""))
+    name <- paste(traceFilename, 
+                  "-interarrivals/it-", 
+                  n, ".txt", sep = "")
+    cont <- (id * 1000) - interarrival[[ n ]]
     write(cont, file = name, append = TRUE) # Add file to lo
-  }
-  interarrival[[as.character(reader[line,3])]] <- (reader[line,1] * 1000)
-}
+    }
+    interarrival[[ n ]] <- ( id * 1000)
+}, reader$V1, reader$V3)
 
 ### Calculate the summary statistics for each key with more than 2 interarrivals.
 
-print('id mean median mid-range gmean std iqr range mad coeficiente_variacion skewness kurtosis')
+cat('id mean median mid-range gmean std iqr range mad coeficiente_variacion skewness kurtosis')
 
 for(k in ls(interarrival)){
   t <- interarrival[[k]]
@@ -67,18 +73,24 @@ for(k in ls(interarrival)){
   if(file.exists(n)){
     v <- as.matrix(read.table(n))
     if(nrow(v) > 1){
-      cat(paste(k, 
-                  mean(v), 
-                  median(v), 
-                  (max(v)-min(v))/2, 
-                  gmean(v),
-                  sd(v),
-                  quantile(v, .75) - quantile(v, .25),
-                  ptp(v),
-                  mad(v), 
-                  var(v),
-                  skewness(v),
-                  kurtosis(v), "\n",sep = " "))
+      sk <- skewness(v)
+      ku <- kurtosis(v)
+      if(is.nan(sk) || is.nan(ku)){
+        write(k, file = "NaN_interarrivals.txt", append = TRUE)
+      }else{
+        cat(paste(k, 
+                    mean(v), 
+                    median(v), 
+                    (max(v)-min(v))/2, 
+                    gmean(v),
+                    sd(v),
+                    quantile(v, .75) - quantile(v, .25),
+                    ptp(v),
+                    mad(v), 
+                    var(v),
+                    sk,
+                    ku, "\n",sep = " "))
+      }
     }
   }
 }
