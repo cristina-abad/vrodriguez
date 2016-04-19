b71251109323cd6e8ab1a5be4f98cb4b29dfb03c
Victor Rodriguez <vix206@gmail.com>
2016-04-19 13:13:09 -0500

    VR - Update scriptGetInterarrivalsSummaryStatistics.R

--- Issue-05-Scripts/R/scriptGetInterarrivalsSummaryStatistics.R
+++ Issue-05-Scripts/R/scriptGetInterarrivalsSummaryStatistics.R
@@ -20,10 +20,9 @@ traceFilename <- ""
 spec <- t(matrix(c('help', 'h', 0, "logical",
           'file', 'f', 1, "character",
           'tracefile', 't', 2, "character"),nrow=4, ncol=3))
-          
-argv <- commandArgs(trailingOnly = TRUE)
+#argv <- commandArgs(trailingOnly = TRUE)
+args <- c("-f","./test/Animation-lookup-100k.txt")
 opts = getopt(spec, args)
-
 if(!is.null(opts$help)){
   print('scriptGetInterarrivalsSummaryStatistics.R -f <tracefile>')
   quit(save = "no", status = 2)
@@ -46,26 +45,29 @@ interarrival <- hash::hash() # Create a hash empty
 ### Open the file in read mode, and parse each line to obtain the timestamp,
 ### the command, and the id.
 
-reader <- fread(input = traceFilename,  sep = " ", header = FALSE)
+# reader <- read.csv(file = traceFilename, sep = " ", header = FALSE)
+reader <- fread(input = traceFilename,  sep = " ", header = FALSE, dec = ".",
+                colClasses = c("string", "string", "string"))
 
-mapply(function(id, n){
+invisible(mapply(function(id, n){
   write(id, 
         file = paste(traceFilename,"-IndiceLeido", sep = ""), 
         append = TRUE) # Add file to log
+  id <- as.numeric(id)
   if(!is.null(interarrival[[ n ]])){
     #dir.create(paste(traceFilename, "-interarrivals", sep = ""))
     name <- paste(traceFilename, 
                   "-interarrivals/it-", 
                   n, ".txt", sep = "")
     cont <- (id * 1000) - interarrival[[ n ]]
-    write(cont, file = name, append = TRUE) # Add file to lo
+    write(toString(cont), file = name, append = TRUE) # Add file to log
     }
-    interarrival[[ n ]] <- ( id * 1000)
-}, reader$V1, reader$V3)
+    interarrival[[ n ]] <- (id * 1000)
+}, reader$V1, reader$V3))
 
 ### Calculate the summary statistics for each key with more than 2 interarrivals.
 
-cat('id mean median mid-range gmean std iqr range mad coeficiente_variacion skewness kurtosis')
+cat('id mean median mid-range gmean std iqr range mad coeficiente_variacion skewness kurtosis\n')
 
 for(k in ls(interarrival)){
   t <- interarrival[[k]]
@@ -78,15 +80,15 @@ for(k in ls(interarrival)){
       if(is.nan(sk) || is.nan(ku)){
         write(k, file = "NaN_interarrivals.txt", append = TRUE)
       }else{
-        cat(paste(k, 
-                    mean(v), 
-                    median(v), 
-                    (max(v)-min(v))/2, 
+        cat(paste(k,
+                    mean(v),
+                    median(v),
+                    (max(v)-min(v))/2,
                     gmean(v),
                     sd(v),
                     quantile(v, .75) - quantile(v, .25),
                     ptp(v),
-                    mad(v), 
+                    mad(v),
                     var(v),
                     sk,
                     ku, "\n",sep = " "))
