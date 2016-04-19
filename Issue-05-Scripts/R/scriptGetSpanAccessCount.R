414ed7ecd41773eba823f3498b9b558864f88916
Victor Rodriguez <vix206@gmail.com>
2016-04-19 14:32:43 -0500

    VR - Update scriptGetSpanAccessCount.R

--- Issue-05-Scripts/R/scriptGetSpanAccessCount.R
+++ Issue-05-Scripts/R/scriptGetSpanAccessCount.R
@@ -1,25 +1,24 @@
-args <- commandArgs(trailingOnly = TRUE)  # Get params in terminal
-f.filename <- args[1]
-if (length(args)==1){
-  # delimiter <- if(!is.na(args[2])) args[2] else " "
-  if(file.exists(f.filename)){
-    f.f1 <- read.table(f.filename, quote="\"", comment.char="", sep = " ", header = F)
-    f.f1 <- f.f1[1]                 # Get first column
-    f.tmp <- f.f1[2:nrow(f.f1),]    # Get kth row until last row
-    print("id span accessCount")
-    
-    for(i in f.tmp){
-      root <- "Animation-traces/it-"
-      f2.filename_id <- paste(root, i, ".txt" ,sep = "") # Concatenate root + filename + ext
-      f2.file <- read.table(f2.filename_id, quote="\"", comment.char="", sep = " ", header = F)
-      f2.interarrivals <- nrow(f2.file)   # Get interarrivals
-      f2.accessCount <- interarrivals + 1
-      f2.span <- sprintf("%.10f", sum(f2.file))
-      cat(sprintf("%d %f %d\n",f2.filename_id, f2.span, f2.accessCount))
-    }
+#### Add libraries neccesaries
+for (package in c('data.table')) {
+  if (!require(package, character.only=T, quietly=T, warn.conflicts = F)) {
+    install.packages(package)
+    library(package, character.only=T, quietly = T, warn.conflicts = F)
   }
-}else{
-  print("Need 1 parameter")
 }
 
+args <- commandArgs(trailingOnly = TRUE)  # Get params in terminal
+f.filename <- args[1]
+f.filenameDataSet <- paste(args[1], "-dataset", sep = "")
 
+f.tmp <- fread(input = f.filenameDataSet, sep = " ", header = TRUE)
+
+cat("id span accessCount\n")
+
+mapply(function(id){
+  f2.filename_id <- paste(f.filename, "-interarrivals/it-", id, ".txt", sep = "")
+  f2.file <- fread(input = f2.filename_id)
+  f2.interarrivals <- nrow(f2.file)   # Get interarrivals
+  f2.accessCount <- f2.interarrivals + 1
+  f2.span <- sprintf("%.10f", sum(f2.file))
+  cat(sprintf("%s %s %s\n", id, f2.span, f2.accessCount))
+}, f.tmp$id)
