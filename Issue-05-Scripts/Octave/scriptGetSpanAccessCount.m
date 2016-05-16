f0b93f16ce03084954834873fad24eb8b54fb5e8
Victor Rodriguez <vix206@gmail.com>
2016-05-16 12:13:39 -0500

    Add files via upload

--- Issue-05-Scripts/Octave/scriptGetSpanAccessCount.m
+++ Issue-05-Scripts/Octave/scriptGetSpanAccessCount.m
@@ -1,20 +1,29 @@
 #! /bin/octave -qf
+warning("off");
+format long;
+
 args = argv();
+filename = args{1};
+filename_ds = strcat(filename, "-dataset");
 if (length(args) == 1)
-	filename = args{1};
-	while(!feof(filename))
-		[filename_id, tmp] = fscanf(filename,"%[^ ] %[^\n]\n","C");
-		filename_id = strcat("Animation-traces/it-", filename_id, ".txt");
-		interarrivals = 0;
-		span = 0;
-		while(!feof(filename_id))
-			[s, tmp] = fscanf(filename,"%f %[^\n]\n","C");
-			interarrivals = interarrivals + 1;
-			span = span + s;
+	filename_tmp = fopen(filename_ds, "r");
+	if(filename_tmp != -1)
+		[filename_id, tmp] = fscanf(filename_tmp,"%[^ ] %[^\n]\n","C");
+		while(!feof(filename_tmp))
+			[filename_id, tmp] = fscanf(filename_tmp,"%[^ ] %[^\n]\n","C");
+			filename_id = strcat(filename,"-interarrivals\/it-", filename_id, ".txt");
+			interarrivals = 0;
+			span = 0;
+			f_id = fopen(filename_id, "r");
+			while(!feof(f_id))
+				[s, tmp] = fscanf(f_id,"%f %[^\n]\n","C");
+				interarrivals = interarrivals + 1;
+				span = span + s;
+			endwhile
+			fclose(f_id);
+			accessCount = interarrivals + 1;
+			printf("%s %.10f %d\n", filename_id, span, accessCount);
 		endwhile
-		accessCount = interarrivals + 1;
-		printf("%s %.10f %d\n", filename_id, span, accessCount);
-	endwhile
-else
-	printf("Need 1 parameter\n");
+		fclose(filename_tmp);
+	endif
 endif
